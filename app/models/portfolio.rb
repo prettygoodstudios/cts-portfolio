29332ccd52f5ed17801530b1c0d35f4af49cf66f
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 15:04:08 +0000

    Implemented Concern for URLS

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  includes Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   
   def self.angular
@@ -9,7 +10,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
   
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x250"
+    self.main_image ||= Portfolio.image_generator(height: '600',width: '400')
+    self.thumb_image ||= Portfolio.image_generator(height: '350',width: '250')
   end
+  
 end
