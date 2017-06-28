ce8ffbbf4f124a7e4dfd0842ef404d17b35220a6
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 17:41:15 +0000

    Implemented front end drag and drop integration.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
 //= require jquery-ui
 //= require html.sortable
 //= require_tree .
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
