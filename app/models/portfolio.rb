3b4d8056839264d11071965785f946186c0ed9b8
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 16:01:53 +0000

    Implement placeholders.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,10 @@
 class Portfolio < ApplicationRecord
   includes Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
   has_many :techonologies
   accepts_nested_attributes_for :techonologies, reject_if: lambda { |attrs| attrs['name'].blank?}
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
   
   def self.angular
     where(subtitle: 'Angular')
@@ -14,11 +13,4 @@ class Portfolio < ApplicationRecord
     order("position ASC")
   end
   scope :ruby_on_rails, -> {where(subtitle: 'Ruby on Rails')}
-  
-  after_initialize :set_defaults
-  
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350',width: '250')
-  end
 end
