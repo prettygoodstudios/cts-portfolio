fd2be3aa8f5e07738e85cd1773091f86465b8a2c
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:20:59 +0000

    Moved the keys to secrets.yml

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,10 +1,10 @@
 module SocialTool
    def self.twitter_search
        client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = "lok6hltcBUBbLj3QjeEWY7yfa"
-        config.consumer_secret     = "2sqb8TMBsc7CXnkAXeJg9WKxV48j7EajRR9WYxqLxxe4mKkluf"
-        config.access_token        = "884418783460302848-dcViOZlio6XYLR0yXXcl6jG3vJacIst"
-        config.access_token_secret = "Ajyvwpbwe8btKlP655hj82mZNFLjmGAvELwZqn6ddUJ7g"
+        config.consumer_key        = Rails.application.secrets.TWITTER_CONSUMER_KEY
+        config.consumer_secret     = Rails.application.secrets.TWITTER_CONSUMER_SECRET
+        config.access_token        = Rails.application.secrets.TWITTER_ACCESS_TOKEN
+        config.access_token_secret = Rails.application.secrets.Twitter_CONSUMER_SECRET
       end
       client.search("#rails", result_type: "recent").take(6).collect do |tweet|
         "#{tweet.user.screen_name}: #{tweet.text}"
