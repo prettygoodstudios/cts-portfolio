f6dd6c1f307db3eeec57be38944734866fc19fd6
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:31:04 +0000

    Implemented displaying tweets.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  
+  get 'tech-news', to: 'pages#tech_news'
   resources :blogs do
     member do
       get :toggle_status
