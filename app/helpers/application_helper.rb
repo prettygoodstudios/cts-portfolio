49c03dc7e4917b4b24f0b0fc4d836bc2b238e38d
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 15:19:27 +0000

    Implemented source tracking feature.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,10 @@ module ApplicationHelper
     end
   end
   
-  def source_helper(layout_name)
+  def source_helper(styles)
     if session[:source]
-      content_tag(:p,"Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} page.", class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path} if you'd like to work together."
+      content_tag(:p,greeting.html_safe, class: styles)
     end
   end
   def copyright_generator
