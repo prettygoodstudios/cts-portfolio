53d40844c0103e0bc6e8814317dd7ba496c3bdc8
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 05:04:36 +0000

    Trying this for debugging purposes.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,7 +38,7 @@ class PortfoliosController < ApplicationController
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
-    @portfolio_item = Portfolio.find(params[:id])
+    p @portfolio_item
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item Is Updated.' }
