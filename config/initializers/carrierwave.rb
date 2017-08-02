e4bc6e8801026212f89dc39f587be0ea8a7f67cb
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 05:14:37 +0000

    Fixed issues with bucket name.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV['_FIGARO_S3_BUCKET_NAME']
+  config.aws_bucket = ENV['S3_BUCKET_NAME']
   config.aws_acl    = 'public-read'
   # The maximum period for authenticated_urls is only 7 days.
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
